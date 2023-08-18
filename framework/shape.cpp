4bf3ba499f1edbb82e7764f931096b821e58a0a3
ANNI90123 <ajngit@mail.n-ithing.de>
2023-08-18 15:27:20 +0200

    Add matrices as attributes and methods scale, rotate and translate to change matrices to class shape

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -1,5 +1,7 @@
 #include "shape.hpp"
 #include <string>
+#define OWN_PI 3.14159265359f
+
 
 Shape::Shape(std::string const& name, std::shared_ptr<Material> const& material) : name_{ name }, material_{ material } {
 };
@@ -24,4 +26,57 @@ std::string Shape::get_name() const {
 
 std::shared_ptr<Material> Shape::get_material() const {
 	return material_;
+}
+
+void Shape::scale(glm::vec3 const& scale_vec) {
+	glm::mat4 scale_mat{};
+	scale_mat[0] = glm::vec4{scale_vec.x, 0.0f, 0.0f, 0.0f};
+	scale_mat[1] = glm::vec4{ 0.0f, scale_vec.y, 0.0f, 0.0f };
+	scale_mat[2] = glm::vec4{ 0.0f, 0.0f, scale_vec.z, 0.0f };
+	scale_mat[3] = glm::vec4{ 0.0f, 0.0f, 0.0f, 1.0f };
+
+	world_transformation_ = scale_mat * world_transformation_;
+	//inverse Matrix
+
+}
+
+void Shape::rotate(float degree, glm::vec3 const& rotation_axis) {
+	float radian = degree * OWN_PI / 180.0f;
+
+	glm::mat4 rotation_mat{};
+	rotation_mat[3] = glm::vec4{ 0.0f, 0.0f, 0.0f, 1.0f };
+
+	if (rotation_axis == glm::vec3{1.0f, 0.0f, 0.0f}) {
+		rotation_mat[0] = glm::vec4{1.0f, 0.0f, 0.0f, 0.0f};
+		rotation_mat[1] = glm::vec4{0.0f, std::cos(radian), std::sin(radian), 0.0f};
+		rotation_mat[2] = glm::vec4{ 0.0f, -1.0f* std::sin(radian), std::cos(radian), 0.0f };
+	}
+	else if (rotation_axis == glm::vec3{ 0.0f, 1.0f, 0.0f }) {
+		rotation_mat[0] = glm::vec4{ std::cos(radian), 0.0f, std::sin(radian), 0.0f };
+		rotation_mat[1] = glm::vec4{ 0.0f, 1.0f, 0.0f, 0.0f };
+		rotation_mat[2] = glm::vec4{ -1.0f * std::sin(radian), 0.0f, std::cos(radian), 0.0f };
+	}
+	else if (rotation_axis == glm::vec3{ 0.0f, 0.0f, 1.0f }) {
+		rotation_mat[0] = glm::vec4{ std::cos(radian), std::sin(radian), 0.0f, 0.0f };
+		rotation_mat[1] = glm::vec4{ -1.0f * std::sin(radian), std::cos(radian), 0.0, 0.0f };
+		rotation_mat[2] = glm::vec4{ 0.0f, 0.0f, 1.0f, 0.0f };
+	}
+
+	world_transformation_ = rotation_mat * world_transformation_;
+	//inverse Matrix
+
+}
+
+
+void Shape::translte(glm::vec3 const& translation_vec) {
+
+	glm::mat4 translation_mat{};
+
+	translation_mat[0] = glm::vec4{1.0f, 0.0f, 0.0f, 0.0f};
+	translation_mat[1] = glm::vec4{ 0.0f, 1.0f, 0.0f, 0.0f };
+	translation_mat[2] = glm::vec4{ 0.0f, 0.0f, 1.0f, 0.0f };
+	translation_mat[0] = glm::vec4{ translation_vec, 1.0f };
+
+	world_transformation_ = translation_mat * world_transformation_;
+	//inverse Matrix
 }
