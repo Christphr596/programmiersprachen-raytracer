c16e9c55fbc3bb138e976086d75e884dd46dfa46
ANNI90123 <ajngit@mail.n-ithing.de>
2023-08-19 08:03:11 +0200

    Add helpfunction transform(glm::mat4, Ray) to transform rays

---
+++ framework/Ray.cpp
@@ -0,0 +1,14 @@
+#include <glm/glm.hpp>
+#include "Ray.hpp"
+
+Ray transform(glm::mat4 const& mat, Ray const& r) {
+	glm::vec4 origin{ r.origin, 1.0f };
+	glm::vec4 direction{ r.direction, 0.0f };
+
+	origin = mat * origin;
+	direction = mat * direction;
+
+	return Ray{ glm::vec3{origin.x, origin.y, origin.z}, glm::vec3{direction.x, direction.y, direction.z} };
+
+
+}
