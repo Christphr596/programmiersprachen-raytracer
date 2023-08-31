3aa07cb8a6094824756e0590ac5065bc9b9726dc
ANNI90123 <ajngit@mail.n-ithing.de>
2023-08-31 10:11:53 +0200

    Add intersection method to class composite

--- framework/composite.cpp
+++ framework/composite.cpp
@@ -19,21 +19,37 @@
 Composite::Composite(std::string const& name) : Shape{ name, nullptr } {
 };
 
+
 glm::vec3 Composite::normale(glm::vec3 const& point) {
 	glm::vec3 vec{ 0,0,0 };
 	return vec;
 }
 
-HitPoint Composite::intersect(Ray const& ray) {
-	HitPoint hitpoint;
-	return hitpoint;
+HitPoint Composite::intersect(Ray const& r_original) {
+    Ray r = transform(Shape::get_w_t_inv_mat(), r_original);
+
+    HitPoint closest_hp{};
+
+    for (auto s : children_) {
+        HitPoint hp = s->intersect(r);
+        if (hp.cut) {
+            if (hp.distance < closest_hp.distance) {
+                closest_hp = hp;
+                //closest_s = s;
+            }
+        }
+
+    }
+    closest_hp = transform(Shape::get_w_t_mat(), closest_hp);
+    return closest_hp;
+
 }
 
 void Composite::add_shape(std::shared_ptr<Shape> shape) {
-	container.push_back(shape);
+	children_.push_back(shape);
 };
 
 std::vector<std::shared_ptr<Shape>> Composite::get_children()const {
 
-	return container;
+	return children_;
 };
