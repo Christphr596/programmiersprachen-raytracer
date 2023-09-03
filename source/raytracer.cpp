89237050acb61fccf84c7a2b9a3dc87455e87309
ANNI90123 <ajngit@mail.n-ithing.de>
2023-09-03 15:49:20 +0200

    Write and render sdfs

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -1,43 +1,88 @@
 #include <renderer.hpp>
 #include <window.hpp>
-
+#include <fstream>
+#include <ostream>
+#include <iostream>
 #include <GLFW/glfw3.h>
 #include <thread>
 #include <utility>
 #include <cmath>
 #include "scene.hpp"
 
+
+
+int main(int argc, char* argv[])
+{
+    unsigned const image_width =  800;
+    unsigned const image_height = 600;
+
+
+    std::ifstream main_source("../../video.sdf");//Quelldatei in Binär öffnen
+
+    for (int i = 105; i <= 135; i++) {
+        std::string const filename = "./image" + std::to_string(i) + ".ppm";
+        main_source.seekg(0);
+        std::ofstream image_source("../image" + std::to_string(i) + ".sdf", std::ios::trunc); //neue Datei auch im binären Modus(für Zeilenumbrüche)
+
+        image_source << main_source.rdbuf();
+
+        image_source << "\n" << "define transform c translate 0 0 " << (0.5 * i);
+        image_source << "\n" << "define transform c rotate " << (i) << " 0 0 1";
+
+        if(i >110 && i <= 130){
+            image_source << "\n" << "define transform bullett translate " << (i -110)*0.25 << " " << (i-110)*0.25 << " 0";
+        }
+
+        if (i > 130 && i <= 140) {
+            image_source << "\n" << "define transform bullett translate 5 5 0";
+        }
+
+        if (i > 140 && i <= 160) {
+            image_source << "\n" << "define transform bullett translate " << 5- (i - 140) * 0.25 << " " << 5- (i - 140) * 0.25 << " 0";
+        }
+
+        image_source.close();
+
+        Scene s = parse_sdf("../image" + std::to_string(i) + ".sdf");
+
+        Renderer renderer{image_width, image_height, filename,s};
+
+       renderer.render();
+    }
+
+    main_source.close();
+
+    return 0;
+}
+
+/*
 //now single th
 int main(int argc, char* argv[])
 {
-  unsigned const image_width = /*401*/800;
-  unsigned const image_height = /*401*/600;
-  std::string const filename = "./checkerboard.ppm";
+  unsigned const image_width = 800;
+  unsigned const image_height = 600;
+  std::string const filename = "./image.ppm";
   
-  Scene s = parse_sdf("../../test_1.sdf");
 
-  
-  for (auto i: s.shape_container) {
-      std::cout << i->get_name() << "\n";
-
-      std::cout << i->get_w_t_mat()[0][0] << " " << i->get_w_t_mat()[0][1] << " " << i->get_w_t_mat()[0][2] << " " << i->get_w_t_mat()[0][3] << "\n" ;
-      std::cout << i->get_w_t_mat()[1][0] << " " << i->get_w_t_mat()[1][1] << " " << i->get_w_t_mat()[1][2] << " " << i->get_w_t_mat()[1][3] << "\n";
-      std::cout << i->get_w_t_mat()[2][0] << " " << i->get_w_t_mat()[2][1] << " " << i->get_w_t_mat()[2][2] << " " << i->get_w_t_mat()[2][3] << "\n";
-      std::cout << i->get_w_t_mat()[3][0] << " " << i->get_w_t_mat()[3][1] << " " << i->get_w_t_mat()[3][2] << " " << i->get_w_t_mat()[3][3] << "\n";
-  }
-  
-  Renderer renderer{image_width, image_height, filename,s/* parse_sdf("C:\\Users\\Annika\\00UNI\\SE1\\07Raytracer\\programmiersprachen-raytracer\\test_1.sdf")*/};
-
+  Scene s = parse_sdf("../../video.sdf");
+  Renderer renderer{image_width, image_height, filename,s };
   renderer.render();
 
+
+ 
+
+
+  
   Window window{{image_width, image_height}};
 
   while (!window.should_close()) {
     if (window.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
       window.close();
     }
+
     window.show(renderer.color_buffer());
   }
 
   return 0;
-}
+}*/
+
